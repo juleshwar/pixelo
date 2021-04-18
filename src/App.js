1a20b456e426b51a5de64ab224eaf9a83f172bf6
juleshwar.babu <juleshwar.babu@zohocorp.com>
2021-04-18 18:14:50 +0530

    refactor: getting rid of existing css files
    gonna use tailwind yo!

--- src/App.js
+++ src/App.js
@@ -3,24 +3,24 @@ import GameView from "./components/GameView";
 import PaletteBar from "./components/PaletteBar";
 
 function App() {
-    return (
-        <div className="App">
-            <PaletteBar
-                colors={[
-                    "#FFFFFF",
-                    "#000000",
-                    "#9CA3AF",
-                    "#FBBF24",
-                    "#34D399",
-                    "#60A5FA",
-                    "#818CF8",
-                    "#A78BFA",
-                    "#F472B6",
-                ]}
-            />
-            <GameView />
-        </div>
-    );
+  return (
+    <div>
+      <PaletteBar
+        colors={[
+          "#FFFFFF",
+          "#000000",
+          "#9CA3AF",
+          "#FBBF24",
+          "#34D399",
+          "#60A5FA",
+          "#818CF8",
+          "#A78BFA",
+          "#F472B6",
+        ]}
+      />
+      <GameView />
+    </div>
+  );
 }
 
 export default App;
--- src/components/ColorCell.js
+++ src/components/ColorCell.js
@@ -1,16 +1,15 @@
 import React, { Component } from "react";
-import "../styles/components/ColorCell.css";
 
 export class ColorCell extends Component {
-    render() {
-        const color = "black";
-        return (
-            <div
-                className="color-cell"
-                style={Object.assign({}, { background: color })}
-            ></div>
-        );
-    }
+  render() {
+    const color = "black";
+    return (
+      <div
+        className="h-16 w-16"
+        style={Object.assign({}, { background: color })}
+      ></div>
+    );
+  }
 }
 
 export default ColorCell;
