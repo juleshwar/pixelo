3c229f9ee40fbe22b5cd13ac78721a2e8a22f973
juleshwar.babu <juleshwar.babu@zohocorp.com>
2021-04-18 19:43:19 +0530

    feat: adding two panels in GameView

--- src/components/GameView.js
+++ src/components/GameView.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
+import { DRAWING_3 } from "../constants/components/DrawingPanelConstants";
+
 import DrawingPanel from "./DrawingPanel";
 
 export class GameView extends Component {
-    render() {
-        return (
-            <>
-                <DrawingPanel />
-            </>
-        );
-    }
+  render() {
+    return (
+      <div className="flex">
+        <DrawingPanel drawingMeta={DRAWING_3} />
+        <DrawingPanel />
+      </div>
+    );
+  }
 }
 
 export default GameView;
