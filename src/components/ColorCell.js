01346f661ba1271d6ddf592d02a01a8262d93373
juleshwar.babu <juleshwar.babu@zohocorp.com>
2021-04-18 19:12:07 +0530

    feat: modifying ColorCell to be reused more generally

--- src/components/ColorCell.js
+++ src/components/ColorCell.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export class ColorCell extends Component {
+  static propTypes = {
+    size: PropTypes.number.isRequired,
+    color: PropTypes.string.isRequired,
+  };
   render() {
-    const color = "black";
     return (
       <div
-        className="h-16 w-16"
-        style={Object.assign({}, { background: color })}
+        className={`h-${this.props.size} w-${this.props.size}`}
+        style={Object.assign({}, { background: this.props.color })}
       ></div>
     );
   }
