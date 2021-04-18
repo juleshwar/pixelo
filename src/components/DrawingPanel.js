7993d5b0795f7ee283348441329db53cf7a7c100
juleshwar.babu <juleshwar.babu@zohocorp.com>
2021-04-19 00:50:16 +0530

    feat: adding functionality to draw in a panel
    1. on mouse move over cell, the DrawingPanel triggers onUpdateCellColor
    2. GameView's onUpdateColor updates the color of the currentMeta at the given index

--- src/components/DrawingPanel.js
+++ src/components/DrawingPanel.js
@@ -4,13 +4,30 @@ import PropTypes from "prop-types";
 import { DRAWING_0 } from "../constants/components/DrawingPanelConstants";
 
 export class DrawingPanel extends Component {
+  constructor(props) {
+    super(props);
+    this.handleMouseMoveOnColorCell = this.handleMouseMoveOnColorCell.bind(
+      this
+    );
+  }
+
   static propTypes = {
     drawingMeta: PropTypes.arrayOf(PropTypes.string),
+    isReadOnly: PropTypes.bool,
+    onUpdateCellColor: PropTypes.func,
   };
 
   static defaultProps = {
     drawingMeta: DRAWING_0,
+    isReadOnly: true,
   };
+
+  handleMouseMoveOnColorCell(cellIndex, e) {
+    // only allow when left button is pressed
+    if (e.buttons === 1) {
+      this.props.onUpdateCellColor(cellIndex);
+    }
+  }
 
   render() {
     const gridSize = 10;
@@ -21,8 +38,16 @@ export class DrawingPanel extends Component {
         const cellIndex = colIndex * gridSize + rowIndex;
         const cellColor = this.props.drawingMeta[cellIndex];
         rowCells.push(
-          <td key={cellIndex} className="p-0 border border-gray-200">
-            <ColorCell color={cellColor} size={16} />
+          <td
+            key={cellColor + cellIndex}
+            className="p-0 border border-gray-200"
+            onMouseMove={
+              this.props.isReadOnly
+                ? null
+                : this.handleMouseMoveOnColorCell.bind(this, cellIndex)
+            }
+          >
+            <ColorCell index={cellIndex} color={cellColor} size={16} />
           </td>
         );
       }
--- src/components/GameView.js
+++ src/components/GameView.js
@@ -1,14 +1,46 @@
 import React, { Component } from "react";
-import { DRAWING_3 } from "../constants/components/DrawingPanelConstants";
-
+import {
+  DRAWING_0,
+  DRAWING_3,
+} from "../constants/components/DrawingPanelConstants";
+import PropTypes from "prop-types";
 import DrawingPanel from "./DrawingPanel";
 
 export class GameView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentMeta: DRAWING_0,
+    };
+    this.onUpdateColor = this.onUpdateColor.bind(this);
+  }
+
+  static propTypes = {
+    selectedColor: PropTypes.string,
+  };
+
+  static defaultProps = {
+    selectedColor: "#FF00FF",
+  };
+
+  onUpdateColor(colorIndex) {
+    const modifiedMeta = JSON.parse(JSON.stringify(this.state.currentMeta));
+    modifiedMeta[colorIndex] = this.props.selectedColor;
+    this.setState({ currentMeta: modifiedMeta });
+  }
+
   render() {
     return (
       <div className="flex">
-        <DrawingPanel drawingMeta={DRAWING_3} />
-        <DrawingPanel />
+        <DrawingPanel
+          drawingMeta={DRAWING_3}
+          onUpdateCellColor={this.onUpdateColor}
+        />
+        <DrawingPanel
+          drawingMeta={this.state.currentMeta}
+          onUpdateCellColor={this.onUpdateColor}
+          isReadOnly={false}
+        />
       </div>
     );
   }
