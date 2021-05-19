1b12d90c360aa6e677034718f86861654a8de83b
juleshwar.babu <juleshwar.babu@zohocorp.com>
2021-05-19 13:59:38 +0530

    feat: adding a design route
    1. Drawings can be submitted via this route
    2. Check if GameView can be repurposed for this

---
+++ src/routes/PageDesignDrawing.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import DesignDrawing from "../components/DesignDrawing";
+
+export class PageDesignDrawing extends Component {
+  render() {
+    return <DesignDrawing />;
+  }
+}
+
+export default PageDesignDrawing;
