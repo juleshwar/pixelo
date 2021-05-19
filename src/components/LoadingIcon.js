d318cc0d0da17cd576e0522721fb83dcbbbab0cd
juleshwar.babu <juleshwar.babu@zohocorp.com>
2021-05-19 12:38:01 +0530

    fix: adding a flag to stop animation when the component has been unmounted
    Animation doesn't stop even as the code is async. I also learnt we can't rely on setState for making immediate changes as it is an async method.

--- src/components/LoadingIcon.js
+++ src/components/LoadingIcon.js
@@ -5,6 +5,7 @@ export class LoadingIcon extends Component {
   constructor(props) {
     super(props);
     this.pixeloIconRef = React.createRef();
+    this.doAnimate = true;
   }
 
   async delay(timeInMs) {
@@ -29,6 +30,14 @@ export class LoadingIcon extends Component {
     let paletteIndex = 0;
     const getCharCode = (char) => char.charCodeAt(0);
     const animateSquareAtIndex = async (i) => {
+      if (!this.doAnimate) {
+        /**
+         * This is to abruptly stop the subsequent async code from running as the current component has been
+         * removed from the DOM already
+         */
+        throw new Error("No need to animate no more");
+      }
+
       const squareAtIndex = pixeloIconSvg.querySelector(
         `#${String.fromCharCode(i)}`
       );
@@ -38,22 +47,34 @@ export class LoadingIcon extends Component {
     };
 
     for (let i = getCharCode("a"); i <= getCharCode("j"); i++, paletteIndex++) {
-      await animateSquareAtIndex(i);
+      try {
+        await animateSquareAtIndex(i);
+      } catch (error) {
+        // noop
+      }
     }
     for (let i = getCharCode("c"); i >= getCharCode("a"); i--, paletteIndex++) {
-      await animateSquareAtIndex(i);
+      try {
+        await animateSquareAtIndex(i);
+      } catch (error) {
+        // noop
+      }
     }
     await this.delay(ANIMATION_DELAY);
   }
 
   async loopPixeloIconAnimation() {
-    while (1) {
+    while (this.doAnimate) {
       await this.animatePixeloIcon();
     }
   }
 
   componentDidMount() {
     this.loopPixeloIconAnimation();
+  }
+
+  componentWillUnmount() {
+    this.doAnimate = false;
   }
 
   render() {
