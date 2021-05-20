c20ea06b2f18370aa1f12768200e3ac8d5909b57
juleshwar.babu <juleshwar.babu@zohocorp.com>
2021-05-21 00:25:43 +0530

    perf: ⚡️ moving cleanSlate definition to client

--- src/services/PixeloStateHandler.js
+++ src/services/PixeloStateHandler.js
@@ -7,8 +7,10 @@ class PixeloStateHandlerBean {
   constructor() {
     this.state = {
       drawingsDataLoading: false,
-      colorsDataLoading: false,
-      DRAWINGS: [],
+      DRAWINGS: {
+        cleanSlate: Array(100).fill(this.COLOR_PALETTE_MAP["WHITE"]),
+        presetDrawings: [],
+      },
     };
   }
 
@@ -26,44 +28,28 @@ class PixeloStateHandlerBean {
 
   async getDrawings() {
     if (
-      !Object.entries(this.state.DRAWINGS).length &&
+      !this.state.DRAWINGS.presetDrawings.length &&
       !this.state.drawingsDataLoading
     ) {
-      let { presetDrawings, cleanSlate } = await this._fetchDrawings();
-      cleanSlate = cleanSlate.map(
-        (colorCode) => this.COLOR_PALETTE_MAP[colorCode]
-      );
+      let presetDrawings = await this._fetchPresetDrawings();
       presetDrawings = presetDrawings.map((drawing) =>
         drawing.map((colorCode) => this.COLOR_PALETTE_MAP[colorCode])
       );
 
-      this.state.DRAWINGS = { cleanSlate, presetDrawings };
-      return { cleanSlate, presetDrawings };
-    } else {
-      return this.state.DRAWINGS;
+      this.state.DRAWINGS.presetDrawings = presetDrawings;
     }
+    return this.state.DRAWINGS;
   }
 
-  async _fetchDrawings() {
+  async _fetchPresetDrawings() {
     this.state.drawingsDataLoading = true;
     const presetDrawingsResponse = await AirtableService.fetchData(
       PRESET_DRAWINGS_BASE_NAME
     );
     this.state.drawingsDataLoading = false;
 
-    return presetDrawingsResponse.reduce(
-      (accumulator, record) => {
-        const name = record.get("name");
-        const template = JSON.parse(record.get("template"));
-        if (name === "CLEAN_SLATE") {
-          accumulator.cleanSlate = template;
-        } else {
-          accumulator.presetDrawings.push(template);
-        }
-
-        return accumulator;
-      },
-      { presetDrawings: [] }
+    return presetDrawingsResponse.map((record) =>
+      JSON.parse(record.get("template"))
     );
   }
 }
