5329f4acf1a2d07248ccdc3b6ef0ebd297212346
juleshwar.babu <juleshwar.babu@zohocorp.com>
2021-05-24 01:24:52 +0530

    refactor: 🔧 updating z-index values

--- tailwind.config.js
+++ tailwind.config.js
@@ -33,6 +33,19 @@ module.exports = {
       desktop: "1280px",
       "desktop-xl": "1536px" /* External Displays */,
     },
+    zIndex: {
+      0: 0,
+      1: 1,
+      2: 2,
+      3: 3,
+      4: 4,
+      5: 5,
+      6: 6,
+      7: 7,
+      8: 8,
+      9: 9,
+      10: 10,
+    },
   },
   variants: {
     extend: {
