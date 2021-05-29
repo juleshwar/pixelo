a3fe8deffb37311b871f4eca7ab9a9fb6b529560
juleshwar.babu <bjuleshwar@gmail.com>
2021-05-29 21:07:16 +0530

    feat: ✨ listing settings on settings page
    Still need to handle styles

--- src/routes/RouteSettings.js
+++ src/routes/RouteSettings.js
@@ -1,8 +1,38 @@
 import React, { Component } from "react";
+import { LocalStorageService } from "../services/LocalStorageService";
 
 export class RouteSettings extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pixeloSettings: LocalStorageService.pixeloSettings,
+    };
+  }
+  onChangeSetting(settingKey) {
+    LocalStorageService.toggleSetting(settingKey);
+    this.setState({ pixeloSettings: LocalStorageService.pixeloSettings });
+  }
   render() {
-    return <div></div>;
+    const pixeloSettings = this.state.pixeloSettings;
+    const listItems = [];
+    for (const settingKey in pixeloSettings) {
+      if (Object.hasOwnProperty.call(pixeloSettings, settingKey)) {
+        const settingValue = pixeloSettings[settingKey];
+        listItems.push(
+          <li key={settingKey}>
+            {settingKey}:
+            <button onClick={this.onChangeSetting.bind(this, settingKey)}>
+              {settingValue}
+            </button>
+          </li>
+        );
+      }
+    }
+    return (
+      <section className="h-full grid place-items-center place-content-center">
+        <ul>{listItems}</ul>
+      </section>
+    );
   }
 }
 
